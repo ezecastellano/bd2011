9fab216844c425340aa33eed5802cd2db1adbfaa
pablo.brusco@gmail.com <pablo.brusco@gmail.com@21122e01-fad2-d5ea-7797-12c651ce134f>
2011-05-01 04:09:10 +0000

    auditorias, triggers

---
+++ trunk/tp1/triggers.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS `mydb`.`auditoria`;
+
+CREATE TABLE `mydb`.`auditoria`
+(
+   id int NOT NULL AUTO_INCREMENT,
+   idmedida int,
+   descripcion varchar(50),
+   valor_anterior varchar(50),
+   valor_nuevo varchar(50),
+   usuario varchar(40),
+   modificado datetime,
+   PRIMARY KEY(id)
+) ENGINE = InnoDB;
+
+
+delimiter $$
+
+DROP TRIGGER medidaUpdate$$
+CREATE TRIGGER medidaUpdate after UPDATE ON medida
+   FOR each row
+    BEGIN
+         INSERT INTO auditoria (idmedida, descripcion, valor_anterior , valor_nuevo, usuario  , modificado ) VALUES(OLD.idmedida,'UPDATED' , OLD.idnorma , NEW.idnorma, CURRENT_USER(), NOW() );
+    END;$$
+
+
+DROP TRIGGER medidaDelete$$
+CREATE TRIGGER medidaDelete after DELETE ON medida
+   FOR each row
+    BEGIN
+         INSERT INTO auditoria (idmedida, descripcion, valor_anterior ,  usuario  , modificado ) VALUES(OLD.idmedida,'DELETED' ,OLD.idnorma ,  CURRENT_USER(), NOW() );
+    END;$$
+
+
+DROP TRIGGER medidaInsert$$
+CREATE TRIGGER medidaInsert after INSERT ON medida
+   FOR each row
+    BEGIN
+      INSERT INTO auditoria (idmedida, descripcion, valor_nuevo, usuario  , modificado ) VALUES (NEW.idmedida,'INSERTED',  NEW.idnorma, CURRENT_USER(), NOW() );
+    END;$$
+
+
+
+
+delimiter ;
+
