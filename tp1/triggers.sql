c143ac70b3343f290310cd6ca33650b25a7a2cd4
marianobianchi08 <marianobianchi08@21122e01-fad2-d5ea-7797-12c651ce134f>
2011-05-04 17:42:38 +0000

    Actualice el trigger que estaba separado a como quedo en la base

--- tp1/triggers.sql
+++ tp1/triggers.sql
@@ -1,36 +1,46 @@
 DROP TABLE IF EXISTS `mydb`.`auditoria`;
 
-CREATE TABLE `mydb`.`auditoria`
-(
-   id int NOT NULL AUTO_INCREMENT,
-   idmedida int,
-   descripcion varchar(50),
-   valor_anterior varchar(50),
-   valor_nuevo varchar(50),
-   usuario varchar(40),
-   modificado datetime,
-   PRIMARY KEY(id)
-) ENGINE = InnoDB;
+CREATE TABLE  `mydb`.`auditoria` (
+  `idauditoria` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(30) NOT NULL,
+  `descripcion` varchar(20) DEFAULT NULL,
+  `fecha_alteracion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `idmedida_nueva` int(11) DEFAULT NULL,
+  `idmedida_vieja` int(11) DEFAULT NULL,
+  `idnorma_nueva` int(11) DEFAULT NULL,
+  `idnorma_vieja` int(11) DEFAULT NULL,
+  `idtipo_nueva` int(11) DEFAULT NULL,
+  `idtipo_vieja` int(11) DEFAULT NULL,
+  `tipo_nuevo` enum('P','PP') DEFAULT NULL,
+  `tipo_viejo` enum('P','PP') DEFAULT NULL,
+  `desde_nuevo` timestamp NULL DEFAULT NULL,
+  `desde_viejo` timestamp NULL DEFAULT NULL,
+  `hasta_nuevo` timestamp NULL DEFAULT NULL,
+  `hasta_viejo` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`idauditoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-delimiter $$
 
-DROP TRIGGER IF EXISTS medidaUpdate$$
-CREATE TRIGGER medidaUpdate after UPDATE ON medida
-   FOR each row
-    BEGIN
-         INSERT INTO auditoria (idmedida, descripcion, valor_anterior , valor_nuevo, usuario  , modificado ) VALUES(OLD.idmedida,'UPDATED' , OLD.idnorma , NEW.idnorma, CURRENT_USER(), NOW() );
-    END;$$
+DELIMITER $$
 
-DROP TRIGGER IF EXISTS medidaDelete$$
-CREATE TRIGGER medidaDelete after DELETE ON medida
-   FOR each row
-    BEGIN
-         INSERT INTO auditoria (idmedida, descripcion, valor_anterior ,  usuario  , modificado ) VALUES(OLD.idmedida,'DELETED' ,OLD.idnorma ,  CURRENT_USER(), NOW() );
-    END;$$
+CREATE DEFINER = `root`@`localhost` TRIGGER  `mydb`.`medidaInsert` AFTER INSERT ON `medida` FOR EACH ROW BEGIN
+	INSERT INTO auditoria (usuario, descripcion, fecha_alteracion, idmedida_nueva, idnorma_nueva, idtipo_nueva, tipo_nuevo, desde_nuevo, hasta_nuevo) VALUES (CURRENT_USER(), 'INSERT', NOW(), NEW.idmedida, NEW.idnorma, NEW.idtipo, NEW.tipo, NEW.desde, NEW.hasta);
+END $$
 
-DROP TRIGGER IF EXISTS medidaInsert$$
-CREATE TRIGGER medidaInsert after INSERT ON medida
-   FOR each row
-    BEGIN
-      INSERT INTO auditoria (idmedida, descripcion, valor_nuevo, usuario  , modificado ) VALUES (NEW.idmedida,'INSERTED',  NEW.idnorma, CURRENT_USER(), NOW() );
-    END;$$
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE DEFINER = `root`@`localhost` TRIGGER  `mydb`.`medidaUpdate` AFTER UPDATE ON `medida` FOR EACH ROW BEGIN
+	INSERT INTO auditoria (usuario, descripcion, fecha_alteracion, idmedida_nueva, idmedida_vieja, idnorma_nueva, idnorma_vieja, idtipo_nueva, idtipo_vieja, tipo_nuevo, tipo_viejo, desde_nuevo, desde_viejo, hasta_nuevo, hasta_viejo) VALUES (CURRENT_USER(), 'UPDATE', NOW(), NEW.idmedida, OLD.idmedida, NEW.idnorma, OLD.idnorma, NEW.idtipo, OLD.idtipo, NEW.tipo, OLD.tipo, NEW.desde, OLD.desde, NEW.hasta, OLD.hasta);
+END $$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE DEFINER = `root`@`localhost` TRIGGER  `mydb`.`medidaDelete` AFTER DELETE ON `medida` FOR EACH ROW BEGIN
+	INSERT INTO auditoria (usuario, descripcion, fecha_alteracion, idmedida_vieja, idnorma_vieja, idtipo_vieja, tipo_viejo, desde_viejo, hasta_viejo) VALUES (CURRENT_USER(), 'DELETE', NOW(), OLD.idmedida, OLD.idnorma, OLD.idtipo, OLD.tipo, OLD.desde, OLD.hasta);
+END $$
+
+DELIMITER ;
