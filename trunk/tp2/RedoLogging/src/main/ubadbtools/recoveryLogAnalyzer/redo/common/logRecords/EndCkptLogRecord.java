a3ac9a6bcc7087944d2c28cfb2859ae9b26acfde
juliandondero@gmail.com <juliandondero@gmail.com@21122e01-fad2-d5ea-7797-12c651ce134f>
2011-06-30 18:03:36 +0000

    no se si desordene algo en la svn perdón. Agregue el algoritmo de REDO, mas lo necesario para que aparezca en la pantalla grafica del analizar log. Por el momento te dice de rehacer todas, en el orden correcto, despues podriamos ver que si una pisa otra que rehizo una anterior, la anterior ni mostrarla. Tambien agregue las clases del start y end ckpt, kevin podes usarlas, por ahi tenes que agregarle algun metodo, no se. En los Start hay un metodo getTransactions que devuelve la lista de las transacciones. Abrazo! juli

---
+++ trunk/tp2/RedoLogging/src/main/ubadbtools/recoveryLogAnalyzer/redo/common/logRecords/EndCkptLogRecord.java
@@ -0,0 +1,23 @@
+package ubadbtools.recoveryLogAnalyzer.redo.common.logRecords;
+
+public class EndCkptLogRecord extends RecoveryLogRecord
+{
+
+	public String toString()
+	{
+			return "< END CKPT >";
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		return true;
+		
+	}
+}
